5643a2bdb49b4457e9631f1367c7b974a9512b31
Petter <petter.blomkvist.pb@gmail.com>
2025-01-17 13:09:08 +0100

    nvm this gotta be smooth enough

--- src/app/components/EmoteCodeMirrorExtension.tsx
+++ src/app/components/EmoteCodeMirrorExtension.tsx
@@ -3,14 +3,26 @@ import { EmoteHelper, Emote } from "./EmoteHelper"
 import { ViewUpdate, ViewPlugin, DecorationSet, WidgetType, MatchDecorator, Decoration, EditorView } from "@codemirror/view"
 
 class EmoteWidget extends WidgetType {
-    constructor(readonly emote: Emote) {
+    constructor(readonly emote: Emote, readonly lineHeight: number) {
         super()
     }
 
     toDOM() {
+        let minHeight = 0
+        let maxHeight = 999999
+        this.emote.images.forEach((e) => {
+            if (e.height < this.lineHeight) {
+                minHeight = Math.max(minHeight, e.height)
+            } else {
+                maxHeight = Math.min(maxHeight, e.height)
+            }
+        })
+        const height = maxHeight === 999999 ? minHeight : maxHeight
+        const image = this.emote.images.filter(i => i.height === height)[0]
+        const wrap = document.createElement("span")
         const box = new Image()
         box.classList.add("cm-emote")
-        box.srcset = this.emote.images.map(ei => `${ei.url} ${ei.width}w`).join(', ')
+        box.src = image.url
         box.style.verticalAlign = 'middle'
         box.style.marginBottom = "auto"
         box.style.marginTop = "auto"
@@ -26,10 +38,11 @@ class EmoteWidget extends WidgetType {
 
 const emoteMatcher = (emoteHelper: EmoteHelper) => new MatchDecorator({
     regexp: new RegExp(emoteHelper.getRegExp(), 'g'),
-    decoration: (match) => {
+    decoration: (match, view,) => {
         return Decoration.replace({
             widget: new EmoteWidget(
-                emoteHelper.getEmote(match[0])
+                emoteHelper.getEmote(match[0]),
+                view.defaultLineHeight
             ),
         });
     },
